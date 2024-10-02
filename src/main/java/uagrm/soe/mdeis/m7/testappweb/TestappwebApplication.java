b6d68d9ef9d74e9cccacfaddd200cae2dab738c2
Gustavo Vargas Miranda <gvmgust@gmail.com>
2024-10-02 17:21:22 -0400

    initial commit

---
+++ src/main/java/uagrm/soe/mdeis/m7/testappweb/TestappwebApplication.java
@@ -0,0 +1,13 @@
+package uagrm.soe.mdeis.m7.testappweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestappwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestappwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uagrm/soe/mdeis/m7/testappweb/TestappwebApplicationTests.java
@@ -0,0 +1,13 @@
+package uagrm.soe.mdeis.m7.testappweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestappwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
